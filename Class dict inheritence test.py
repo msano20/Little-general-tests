82be9dc51f38663615de095b64a9cf915973c9b1
Misumi Sano <76826758+msano20@users.noreply.github.com>
2022-09-23 19:16:21 -0400

    Add files via upload

--- Class dict inheritence test.py
+++ Class dict inheritence test.py
@@ -2,39 +2,40 @@
 """
 Created on Fri Sep 23 17:54:56 2022
 
--testClass = setUp Class
 @author: misum
 """
 
 class testClass():
-    def __init__(self,n_dict,key,value):
-        self.n_dict = n_dict
-        self.key = key
-        self.value = value
+    def __init__(self):
+
         self.test_list = []
         self.test_dict = {}
         self.test_dict2 = {}
-        self.dict_list = [self.test_dict,self.test_dict2]
+        self.test_dict3 = {}
+        self.dict_list = [self.test_dict,self.test_dict2,self.test_dict3]
+        self.translation_dict = {'test_dict':self.test_dict,'test_dict2':self.test_dict2,'test_dict3':self.test_dict3}
     
     
     def dictIteration(self,n_dict,key,value):
-        print("Starting dictIteration...")
-        print('n_dict: ',n_dict)
-        #for dict_obj in self.dict_list:
-        dict_str = str('self.'+n_dict)    
-        print(dict_str)
-        if dict_str in str(self.dict_list):    
-            print(n_dict)
-        
-
-    
-
-ideal_dict = 'test_dict2'
-
+        self.n_dict = n_dict
+        self.key = key
+        self.value = value
+        print("Starting dict Iteration...")
+        #print('n_dict: ',n_dict)
+        dict_str = str(n_dict)    
+        #print('dict_str:',dict_str)
+        for k, v in self.translation_dict.items():
+            if dict_str == k:    
+                print(k)
+                print(v)
+                v.update({self.key:self.value})
+        #print(self.translation_dict)
 
 
 #testInstance = testClass('u_dict','test_key','test_val')
-testInstance = testClass('test_dict','test_key','test_val')
+testInstance = testClass()
 
 
-testInstance.dictIteration('test_dict','test_key','test_val')
+testInstance.dictIteration('test_dict3','test_key','test_val')
+
+print(testInstance.translation_dict)
